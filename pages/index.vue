99fcdb66ef4e2cb6f6e8d6a959a1ccfc2ac81ccb
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-02 12:42:14 +0530

    idea done

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,22 @@
 <template>
-  <div>
-    Hi
-    <UButton>Button</UButton>
+  <div class="flex items-center px-20 py-5 justify-around">
+    <!-- <p class="text-3xl font-bold underline font-[Poppins]">
+      Type Anything
+    </p> -->
+    <UInput v-model="value"  size="xl" input-class="text-3xl" placeholder="Type something..." variant="none"/>
+    <URange v-model="font_size" :min="0" :max="100"/>
   </div>
+  <section class="flex items-center justify-center min-h-screen">
+    <p class="text-3xl" :style="{ fontSize: `${font_size}rem` }">
+      {{ value }}
+    </p>
+  </section>
 </template>
 
 <script lang="ts" setup>
+
+const value = ref('')
+const font_size = ref(10)
 
 </script>
 
