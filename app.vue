ff0256e1ac77dcfe41f2b114c21af6ff5e60723d
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-02 21:45:37 +0530

    added source and title

--- app.vue
+++ app.vue
@@ -1,4 +1,12 @@
 <script setup lang="ts">
+
+useHead({
+  title: 'Type Anything',
+  meta: [
+    { name: 'description', content: 'Go IDLE with Type Anything' }
+  ]
+})
+
 </script>
 
 <template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,9 @@
 <template>
   <section class="flex items-center w-full">
-    <div class="flex items-center px-20 py-5 w-full justify-around" v-if="!hidden">
+    <div
+      class="flex items-center px-20 py-5 w-full justify-around"
+      v-if="!hidden"
+    >
       <!-- <p class="text-3xl font-bold underline font-[Poppins]">
         Type Anything
       </p> -->
@@ -14,21 +17,35 @@
       />
       <URange v-model="font_size" :min="0" :max="100" class="w-64" />
       <UCheckbox v-model="selected" name="notifications" label="Animation" />
+      <NuxtLink to="https://github.com/ajmalshahabudeen/type-anything" target="_blank">
+        <UButton
+          color="emerald"
+          variant="soft"
+          label="Source"
+          icon="material-symbols-light:arrow-outward-rounded"
+          trailing
+          class="absolute top-5 right-16"
+        />
+      </NuxtLink>
     </div>
     <UButton
-    icon="line-md:watch-loop"
-    size="sm"
-    color="primary"
-    square
-    variant="solid"
-    @click="hidden = !hidden"
-    @mouseover="hover = true"
-    @mouseleave="hover = false"
-    :class="['absolute top-5 right-5', { 'hide': hidden && !hover }]"
-  />
+      icon="line-md:watch-loop"
+      size="sm"
+      color="primary"
+      square
+      variant="solid"
+      @click="hidden = !hidden"
+      @mouseover="hover = true"
+      @mouseleave="hover = false"
+      :class="['absolute top-5 right-5', { hide: hidden && !hover }]"
+    />
   </section>
   <section class="flex items-center justify-center min-h-screen">
-    <p class="text-wrap text-center" :style="{ fontSize: `${font_size}rem` }" v-if="!selected">
+    <p
+      class="text-wrap text-center"
+      :style="{ fontSize: `${font_size}rem` }"
+      v-if="!selected"
+    >
       {{ value }}
     </p>
     <p
@@ -39,7 +56,6 @@
   </section>
 </template>
 
-
 <!-- //--------------------------------------------------Script -->
 
 <script lang="ts" setup>
@@ -48,7 +64,7 @@ import Typed from "typed.js";
 const typedElement = ref(null);
 let typed: any = null;
 
-const value = ref("");
+const value = ref("Type anything...");
 const font_size = ref(10);
 const selected = ref(false);
 const hidden = ref(false);
