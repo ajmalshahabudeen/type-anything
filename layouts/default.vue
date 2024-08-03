ea414090d430d4bfcb97a120d0f5d41cbc46238b
僕は神だ <ajmalshahabudeen@gmail.com>
2024-08-03 14:30:49 +0530

    vercel speed added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
+    <SpeedInsights/>
     <slot />
   </div>
 </template>
 
 <script lang="ts" setup>
-
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 
 <style>
